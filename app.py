9037c8fc845d73326bd704d6006440c3831df82c
piyush-web-pixel <piyushyadav6226@gmail.com>
2026-05-13 19:07:47 +0530

    mlflow intrigated successfuly

--- app.py
+++ app.py
@@ -3,15 +3,27 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion,DataIngestionConfig
 import os
 import sys
+from src.mlproject.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.mlproject.components.model_trainer import ModelTrainer,ModelTrainerConfig
 
 
 if __name__=='__main__':
     logging.info('the execution is started')
 
     try:
-        # data_ingestion_config=DataIngestionConfig()
+       #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
-   
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        data_transformation=DataTransformation()
+        train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     except Exception as e:
+        logging.info("Custom Exception")
         raise CustomException(e,sys)
